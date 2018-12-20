dad7bf012ce8107031154fcaf245a967841a6e98
Oween <34008762+gocrap@users.noreply.github.com>
2018-12-20 16:47:57 -0700

    Update Php-part.php

--- Php-part.php
+++ Php-part.php
@@ -1,16 +1,19 @@
 <?php
-
-//this part off the code posts the sensor data sent from the esp8266 to an html file in the target website
-
-   if( $_REQUEST["var"]) 
+   if( $_REQUEST["endthis"])
+   {
+       //clear doc
+       file_put_contents('dataDisplayer.html', '');
+       
+   }
+   else if( $_REQUEST["var"]) 
    {
         //print var
-        echo "var has a value of var is". $_REQUEST['var'];
+        echo $_REQUEST['var'];
    }
 
 $var1 = $_REQUEST['var'];
 
-$WriteMyRequest="<p> the value of var is ".$var1."</p><br/>";
+$WriteMyRequest=$var1;
 
 file_put_contents('dataDisplayer.html', $WriteMyRequest, FILE_APPEND);
 
